9af4d94026a8b56890aed4891dde3fde71e83583
Felix Bröhl <broehl@glamus.de>
2020-10-30 11:57:07 +0100

    Added contact create method and delete method

--- src/ios/ContactsX.swift
+++ src/ios/ContactsX.swift
@@ -64,62 +64,194 @@ import Contacts
     func create(command: CDVInvokedUrlCommand) {
         _callbackId = command.callbackId;
 
-        let createContactData: AnyObject = command.argument(at: 0)! as AnyObject
+        let createContactData: NSDictionary = command.argument(at: 0)! as! NSDictionary
 
         self.commandDelegate.run {
+            let store = CNContactStore()
+            self.hasPermission { (granted) in
+                guard granted else {
+                    self.returnError(error: ErrorCodes.PermissionDenied);
+                    return;
+                }
 
-            let contact = CNMutableContact()
+                let contact = CNMutableContact();
 
-            // set namePrefix
-            contact.namePrefix = createContactData.namePrefix
+                // set name prefix
+                if let namePrefix = createContactData.value(forKey: "namePrefix") as? String {
+                    contact.namePrefix = namePrefix
+                }
 
-            // set middleName
-            contact.middleName = createContactData.middleName
+                // set first name
+                if let firstName = createContactData.value(forKey: "firstName") as? String {
+                    contact.givenName = firstName
+                }
 
-            // set familyName
-            contact.familyName = createContactData.familyName
+                // set middle name
+                if let middleName = createContactData.value(forKey: "middleName") as? String {
+                    contact.middleName = middleName
+                }
 
-            // set nameSuffix
-            contact.nameSuffix = createContactData.nameSuffix
+                // set family name
+                if let familyName = createContactData.value(forKey: "familyName") as? String {
+                    contact.familyName = familyName
+                }
 
-            // set jobTitle
-            contact.jobTitle = createContactData.jobTitle
+                // set name suffix
+                if let nameSuffix = createContactData.value(forKey: "nameSuffix") as? String {
+                    contact.nameSuffix = nameSuffix
+                }
 
-            // set organizationName
-            contact.organizationName = createContactData.organizationName
+                // set job title
+                if let jobTitle = createContactData.value(forKey: "jobTitle") as? String {
+                    contact.jobTitle = jobTitle
+                }
 
-            // set postalAddresses
-            //contact.postalAddresses.append()
+                // set organization name
+                if let organizationName = createContactData.value(forKey: "organizationName") as? String {
+                    contact.organizationName = organizationName
+                }
 
-            // set emailAddresses
-            //contact.emailAddresses.append()
+                // set postalAddresses
+                if let postalAddresses = createContactData.value(forKey: "postalAddresses") as? NSArray {
+                    for addressData in postalAddresses {
+                        let container = addressData as! NSDictionary
+                        let postalAddress = CNMutablePostalAddress()
+                        if let value = container.value(forKey: "value") as? NSDictionary {
+                            if let street = value.value(forKey: "street") as? String {
+                                postalAddress.street = street
+                            }
+                            if let city = value.value(forKey: "city") as? String {
+                                postalAddress.city = city
+                            }
+                            if let state = value.value(forKey: "state") as? String {
+                                postalAddress.state = state
+                            }
+                            if let postalCode = value.value(forKey: "postalCode") as? String {
+                                postalAddress.postalCode = postalCode
+                            }
+                            if let isoCountryCode = value.value(forKey: "isoCountryCode") as? String {
+                                postalAddress.isoCountryCode = isoCountryCode
+                            }
+                        }
 
-            // set urlAddresses
-            //contact.urlAddresses.append()
+                        let label = container.value(forKey: "label") as? String ?? CNLabelHome
+                        contact.postalAddresses.append(CNLabeledValue(label: label, value: postalAddress))
+                    }
+                }
 
-            // set phoneNumbers
-            //contact.phoneNumbers.append()
+                // set emailAddresses
+                if let emailAddresses = createContactData.value(forKey: "emailAddresses") as? NSArray {
+                    for addressData in emailAddresses {
+                        let container = addressData as! NSDictionary
+                        if let value = container.value(forKey: "value") as? NSString, let label = container.value(forKey: "label") as? String {
+                            contact.emailAddresses.append(CNLabeledValue(label: label, value: value))
+                        }
+                    }
+                }
 
-            // set birthday
-            /*let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-MM-dd"
-            dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-            let date = dateFormatter.date(from: "2015-04-01")
-            let calendar = Calendar.current
-            let components = calendar.dateComponents([.year, .month, .day], from: date!)
-            contact.birthday = components*/
+                // set urlAddresses
+                if let urlAddresses = createContactData.value(forKey: "urlAddresses") as? NSArray {
+                    for addressData in urlAddresses {
+                        let container = addressData as! NSDictionary
+                        if let value = container.value(forKey: "value") as? NSString, let label = container.value(forKey: "label") as? String {
+                            contact.urlAddresses.append(CNLabeledValue(label: label, value: value))
+                        }
+                    }
+                }
 
-            // create image
+                // set phoneNumbers
+                if let phoneNumbers = createContactData.value(forKey: "phoneNumbers") as? NSArray {
+                    for addressData in phoneNumbers {
+                        let container = addressData as! NSDictionary
+                        if let value = container.value(forKey: "value") as? NSString, let label = container.value(forKey: "label") as? String {
+                            contact.phoneNumbers.append(CNLabeledValue(label: label, value: CNPhoneNumber(stringValue: value as String)))
+                        }
+                    }
+                }
 
-            // cordove result
-            let result:CDVPluginResult = CDVPluginResult(status: CDVCommandStatus_OK);
-            self.commandDelegate.send(result, callbackId: self._callbackId)
+                // set birthday
+                if let birthday = createContactData.value(forKey: "birthday") as? String {
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "yyyy-MM-dd"
+                    dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+                    let date = dateFormatter.date(from: birthday)
+                    let calendar = Calendar.current
+                    let components = calendar.dateComponents([.year, .month, .day], from: date!)
+                    contact.birthday = components
+                }
+
+                // set note
+                if let note = createContactData.value(forKey: "note") as? String {
+                    contact.note = note
+                }
+
+                // create image
+                if let encodedImage = createContactData.value(forKey: "image") as? String {
+                    let dataComponents = encodedImage.components(separatedBy: ",")
+                    let mimeType = dataComponents[0]
+                    let dataString = dataComponents[1]
+                    let dataDecoded = Data(base64Encoded: dataString, options: Data.Base64DecodingOptions.ignoreUnknownCharacters)!
+                    let decodedImage: UIImage = UIImage(data: dataDecoded)!
+                    if mimeType == "data:image/jpeg;base64" {
+                        contact.imageData = decodedImage.jpegData(compressionQuality: 1.0)
+                    } else if mimeType == "data:image/png;base64" {
+                        contact.imageData = decodedImage.jpegData(compressionQuality: 1.0)
+                    }
+                }
+
+                let saveRequest = CNSaveRequest()
+                saveRequest.add(contact, toContainerWithIdentifier: nil)
+
+                do {
+                    try store.execute(saveRequest)
+
+                    // cordove result
+                    let result:CDVPluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: contact.identifier);
+                    self.commandDelegate.send(result, callbackId: self._callbackId)
+                } catch let error {
+                    self.returnError(error: ErrorCodes.UnknownError, message: error.localizedDescription)
+                    return;
+                }
+            }
         }
     }
 
     @objc(delete:)
     func delete(command: CDVInvokedUrlCommand) {
+        _callbackId = command.callbackId;
 
+        let contactId: NSString = command.argument(at: 0)! as! NSString
+
+        self.commandDelegate.run {
+            let store = CNContactStore()
+            self.hasPermission { (granted) in
+                guard granted else {
+                    self.returnError(error: ErrorCodes.PermissionDenied);
+                    return;
+                }
+
+                let saveRequest = CNSaveRequest()
+                do {
+                    let predicate = CNContact.predicateForContacts(withIdentifiers: [contactId as String])
+                    let keysToFetch = [CNContactGivenNameKey] as [CNKeyDescriptor]
+                    let contacts = try store.unifiedContacts(matching: predicate, keysToFetch: keysToFetch)
+                    for contact in contacts {
+                        let contactCopy: CNMutableContact = (contact as CNContact).mutableCopy() as! CNMutableContact
+                        saveRequest.delete(contactCopy)
+                        try store.execute(saveRequest)
+                        break
+                    }
+
+                    // cordove result
+                    let result:CDVPluginResult = CDVPluginResult(status: CDVCommandStatus_OK);
+                    self.commandDelegate.send(result, callbackId: self._callbackId)
+
+                } catch let error {
+                    self.returnError(error: ErrorCodes.UnknownError, message: error.localizedDescription)
+                    return;
+                }
+            }
+        }
     }
 
     @objc(hasPermission:)
@@ -153,25 +285,28 @@ import Contacts
     func hasPermission(completionHandler: @escaping (_ accessGranted: Bool) -> Void, requestIfNotAvailable: Bool = false) {
         let store = CNContactStore();
         switch CNContactStore.authorizationStatus(for: .contacts) {
-                case .authorized:
-                    completionHandler(true)
-                case .denied:
-                    completionHandler(false)
-                case .restricted, .notDetermined:
-                    if(requestIfNotAvailable) {
-                        store.requestAccess(for: .contacts) { granted, error in
-                            if granted {
-                                completionHandler(true)
-                            } else {
-                                DispatchQueue.main.async {
-                                    completionHandler(false)
-                                }
+            case .authorized:
+                completionHandler(true)
+            case .denied:
+                completionHandler(false)
+            case .restricted, .notDetermined:
+                if(requestIfNotAvailable) {
+                    store.requestAccess(for: .contacts) { granted, error in
+                        if granted {
+                            completionHandler(true)
+                        } else {
+                            DispatchQueue.main.async {
+                                completionHandler(false)
                             }
                         }
-                    } else {
-                        completionHandler(false)
                     }
+                } else {
+                    completionHandler(false)
                 }
+            @unknown default:
+                self.returnError(error: ErrorCodes.UnknownError, message: "")
+                return;
+        }
     }
 
     func returnError(error: ErrorCodes, message: String = "") {
